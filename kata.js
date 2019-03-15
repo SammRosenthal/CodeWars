474b2c6d43e1738c792b37eb7f22dda767f24729
Sam Rosenthal <samrrosenthal@gmail.com>
2019-03-14 23:14:12 -0700

    finished scramblies

--- kata.js
+++ kata.js
@@ -1,3 +1,32 @@
+// Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
+
+// Notes:
+
+// Only lower case letters will be used (a-z). No punctuation or digits will be included.
+// Performance needs to be considered
+// Input strings s1 and s2 are null terminated.
+// Examples
+// scramble('rkqodlw', 'world') ==> True
+// scramble('cedewaraaossoqqyt', 'codewars') ==> True
+// scramble('katas', 'steak') ==> False
+
+function scramble(str1, str2) {
+   let word = str2.split("");
+   let scramble = str1.split("");
+   let hashTable = {};
+   
+   for (let i = 0; i < word.length; i++){
+     if (!scramble.includes(word[i])){
+       return false;
+     } else {
+       scramble.splice(scramble.indexOf(word[i]), 1);
+     }
+   }
+   return true;
+ }
+ 
+
+
 // You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
 
 // Implement a function likes :: [String] -> String, which must take in input array, containing the names of people who like an item. It must return the display text as shown in the examples:
