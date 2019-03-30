5f44ce92e844b6c74270fd5a58fa6ecd62a65b95
Sam Rosenthal <samrrosenthal@gmail.com>
2019-03-29 17:41:17 -0700

    interest calculator

--- javaKata.java
+++ javaKata.java
@@ -296,3 +296,17 @@ public class Main {
       }
   }
 }
+
+// For loop calculating interest printing to console
+
+public static void main(String[] args) {
+
+  for (int i = 2; i <= 8; i++) {
+      System.out.println(calculateInterest(10000, i));
+  }
+
+}
+
+public static double calculateInterest(double amount, double interestRate){
+  return (amount * (interestRate/100));
+}
