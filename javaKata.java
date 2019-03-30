e6c835a71d19087cb98dbbd3673b5b6d8fefab05
Sam Rosenthal <samrrosenthal@gmail.com>
2019-03-29 17:34:40 -0700

    switch statement checking for letters

--- javaKata.java
+++ javaKata.java
@@ -213,3 +213,27 @@ public class PlayingCat {
         }
     }
 }
+
+
+// Write a switch statement that checks for letters between A - D inclusive
+
+char switchChar = 'A';
+
+        switch (switchChar) {
+            case 'A':
+                System.out.println("The char is A");
+                break;
+            case 'B':
+                System.out.println("The char is B");
+                break;
+            case 'C':
+                System.out.println("The char is C");
+                break;
+            case 'D':
+                System.out.println("The char is D");
+                break;
+            default:
+                System.out.println("The char is not A, B, C, or D");
+                        
+        }
+
