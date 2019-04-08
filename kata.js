3f1a85297e5bf4b85a8700f45d7bd16178429c13
Sam Rosenthal <samrrosenthal@gmail.com>
2019-04-08 12:03:32 -0700

    human readable seconds to clock

--- kata.js
+++ kata.js
@@ -1,3 +1,24 @@
+// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+// The maximum time never exceeds 359999 (99:59:59)
+
+// You can find some examples in the test fixtures.
+
+function humanReadable(seconds) {
+   let sec = seconds % 60;
+   let hour = Math.floor(seconds / 3600); 
+   let min = Math.floor((seconds - (hour * 3600)) / 60);
+   
+   if (min < 10) min = "0" + min;
+   if (hour < 10) hour = "0" + hour;
+   if (sec < 10) sec = "0" + sec;
+   
+   return hour + ":" + min + ":" + sec;
+ }
+
 
 // Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
 
