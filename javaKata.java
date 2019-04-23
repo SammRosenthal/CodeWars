1129963db33c6ada4093cab1dfd87939f5fc2809
Sam Rosenthal <samrrosenthal@gmail.com>
2019-04-22 18:49:03 -0700

    finished find all factors java problem

--- javaKata.java
+++ javaKata.java
@@ -1,4 +1,52 @@
+// Write a method named printFactors with one parameter of type int named number. 
+// If number is < 1, the method should print "Invalid Value".
+// The method should print all factors of the number. A factor of a number is an integer which divides that number wholly (i.e. without leaving a remainder).
+// For example, 3 is a factor of 6 because 3 fully divides 6 without leaving a remainder. In other words 6 / 3 = 2.
 
+// EXAMPLE INPUT/OUTPUT:
+// * printFactors(6); → should print 1 2 3 6
+// * printFactors(32); → should print 1 2 4 8 16 32
+// * printFactors(10); → should print 1 2 5 10
+// * printFactors(-1); → should print "Invalid Value" since number is < 1
+
+// HINT: Use a while or for loop.
+// NOTE: When printing numbers, each number can be in its own line. They don't have to be separated by a space.
+// For example, the printout for printFactors(10); can be:
+
+// 1
+// 2
+// 5
+// 10
+
+// NOTE: The method printFactors should be defined as public static like we have been doing so far in the course.
+// NOTE: Do not add a main method to the solution code.
+
+public class FactorPrinter {
+    public static void printFactors (int named) {
+        if (named < 1) {
+           System.out.println("Invalid Value");
+        } else {
+            for (int i = 1; i <= named; i++) {
+                if (named % i == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
+
+
+// Write a method named hasSharedDigit with two parameters of type int. 
+// Each number should be within the range of 10 (inclusive) - 99 (inclusive). If one of the numbers is not within the range, the method should return false.
+// The method should return true if there is a digit that appears in both numbers, such as 2 in 12 and 23; otherwise, the method should return false.
+
+// EXAMPLE INPUT/OUTPUT:
+// * hasSharedDigit(12, 23); → should return true since the digit 2 appears in both numbers
+// * hasSharedDigit(9, 99); → should return false since 9 is not within the range of 10-99
+// * hasSharedDigit(15, 55); → should return true since the digit 5 appears in both numbers
+
+// NOTE: The method hasSharedDigit should be defined as public static like we have been doing so far in the course.
+// NOTE: Do not add a main method to the solution code.
 
 public class SharedDigit {
     public static boolean hasSharedDigit (int a, int b) {
