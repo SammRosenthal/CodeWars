6826bb91d98c7e3f0ca196c40c3b377e8eb0eb05
Sam Rosenthal <samrrosenthal@gmail.com>
2019-06-20 19:51:53 -0500

    finished sorting gem algo

--- javaKata.java
+++ javaKata.java
@@ -1,6 +1,39 @@
-// Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+// You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
 
- 
+// The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+// Example 1:
+
+// Input: J = "aA", S = "aAAbbbb"
+// Output: 3
+// Example 2:
+
+// Input: J = "z", S = "ZZ"
+// Output: 0
+// Note:
+
+// S and J will consist of letters and have length at most 50.
+// The characters in J are distinct.
+
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        String[] array2 = S.split("");
+        int count = 0;
+        
+        
+        for (String i : array2) {
+            if (J.contains(i)) {
+                count++;
+            }
+        }
+        
+        return count;
+        
+    }
+}
+
+
+// Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
 
 // Example 1:
 
