c51756b942a2ca7ddb17eb8c3b408526d0628ea8
Sam Rosenthal <samrrosenthal@gmail.com>
2019-11-06 18:14:22 -0600

    updated problems from desktop

--- javaKata.java
+++ javaKata.java
@@ -1,3 +1,42 @@
+// Sock Merchant
+// John works at a clothing store. He has a large pile of socks that he must pair them by color for sale.
+// You will be given an array of integers representing the color of each sock. Determine how many pairs of
+// socks with matching colors there are.
+// John works at a clothing store and he's going through a pile of socks to find the number of matching pairs.
+// More specifically, he has a pile of loose socks where each sock is labeled with an integer, , denoting
+// its color. He wants to sell as many socks as possible, but his customers will only buy them in matching
+// pairs. Two socks, and , are a single matching pair if they have the same color ( ).
+// Given and the color of each sock, how many pairs of socks can John sell?
+// Input Format
+// The first line contains an integer , the number of socks.
+// The second line contains space-separated integers describing the colors of the socks in the pile.
+
+
+static int sockMerchant(int n, int[] ar) {
+    Map<Integer, Integer> sockDrawer = new HashMap<>();
+    Integer pairs = 0;
+    for (int i : ar) {
+        if (sockDrawer.containsKey(i)) {
+            sockDrawer.put(i, sockDrawer.get(i) + 1);
+        } else {
+            sockDrawer.put(i, 1);
+        }
+    }
+
+    for (Map.Entry entry : sockDrawer.entrySet()) {
+        while (true) {
+            if (sockDrawer.get(entry.getKey()) > 1) {
+                sockDrawer.put((Integer) entry.getKey(), sockDrawer.get(entry.getKey()) - 2);
+                pairs += 1;
+            } else {
+                break;
+            }
+        }
+    }
+    
+    return pairs;
+}
+
 // You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
 
 // The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
